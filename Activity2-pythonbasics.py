068ea114b2b34d2a951cb40fc08aa6f25ea6130a
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-09-17 14:33:24 +0100

    save in correct file

---
+++ Activity2-pythonbasics.py
@@ -0,0 +1,12 @@
+'''Write a Python program to divide two numbers, using the input from the user, and print the result on the screen.
+
+Expected Output :
+
+Input the first number: 6
+Input the second number: 2
+The division of the first number and the second number is: 3'''
+
+a=int(input("Enter first number ")) #input for first number
+b=int(input("Enter second number ")) #input second number
+
+print(a / b) #divide the first number by the second and print
