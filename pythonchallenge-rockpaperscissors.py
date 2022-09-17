906ef38a68f1dd299b183ffee75313e8fd8bcdaf
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-09-17 22:58:15 +0100

    Added play again loop and score keeper

--- pythonchallenge-rockpaperscissors.py
+++ pythonchallenge-rockpaperscissors.py
@@ -1,10 +1,16 @@
-""" For this challenge you will create a Rock, Paper, and Scissors game in Python without using any external game libraries.
+""" For this challenge you will create a Rock, Paper, and Scissors game in Python
+without using any external game libraries.
 
-In the Rock, Paper and Scissors game the goal is to create a command-line game where a user has the option to choose between Rock, Paper and Scissors and if the user wins the score is added, and at the end when the user finishes the game, the score is shown.
+In the Rock, Paper and Scissors game the goal is to create a command-line game
+where a user has the option to choose between Rock, Paper and Scissors and if
+the user wins the score is added, and at the end when the user finishes the game,
+the score is shown.
 
 The Winning Rules are as follows:
 
-To create the game, you need to take the user’s choice and then you need to compare it with the computer choice which is taken using the random module in Python from a list of choices, and if the user wins then the score will increase by 1.
+To create the game, you need to take the user’s choice and then you need to compare
+ it with the computer choice which is taken using the random module in Python from
+ a list of choices, and if the user wins then the score will increase by 1.
 
 Test Data:
 
@@ -26,6 +32,7 @@ Final Scores:
 Computer: 1
 Player: 0"""
 
+
 def player_choose():
     global player_hand
     player_hand = input("Enter a choice (Rock(r), Paper(p), Scissors(s): ")
@@ -38,10 +45,11 @@ def player_choose():
     else:
         print("Try again, using r, s or p")
 
+
 def computer_choose():
     import random
     global computer_hand
-    num = random.randint(1,3)
+    num = random.randint(1, 3)
     if num == 1:
         computer_hand = "rock"
     elif num == 2:
@@ -49,30 +57,50 @@ def computer_choose():
     else:
         computer_hand = "scissors"
 
+
 def choose():
     player_choose()
     computer_choose()
     print("You chose ", player_hand, ", computer chose ", computer_hand, ".")
 
+
 def play():
+    global computer_score
+    computer_score = 0
+    global player_score
+    player_score = 0
     if player_hand == computer_hand:
         print("Both players chose", player_hand, "it's a tie")
     elif player_hand == "rock":
         if computer_hand == "paper":
+            computer_score += 1
             print("Paper covers rock, you lose!")
         else:
+            player_score += 1
             print("Rock blunts scissors, you win!")
     elif player_hand == "paper":
         if computer_hand == "scissors":
+            computer_score += 1
             print("Scissors cut paper, you lose!")
         else:
+            player_score += 1
             print("paper covers rock, you win!")
     else:
         if computer_hand == "paper":
+            player_score += 1
             print("Scissors cut paper, you win!")
         else:
+            computer_score += 1
             print("Rock blunts scissors, you lose!")
 
 
-choose()
-play()
+while True:
+    choose()
+    play()
+    print("Computer score: ", computer_score, "Player score: ", player_score)
+    play_again = (input("Do you want to play again? (Y/N): "))
+    play_again == "Y"
+    if play_again == "N":
+        break
+
+
