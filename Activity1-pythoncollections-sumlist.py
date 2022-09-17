0d4ef25117d968455d2b5ed6d454b4739440a1d1
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-09-17 14:12:41 +0100

    Created code to sum list

---
+++ Activity1-pythoncollections-sumlist.py
@@ -0,0 +1,12 @@
+"""Activity 1 - Python Collections
+Write a Python program to sum values of a list.
+
+Test Data:
+
+Sample List: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Expected Output: The sum is 55."""
+
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = sum(mylist)
+print("The sum is", total)
